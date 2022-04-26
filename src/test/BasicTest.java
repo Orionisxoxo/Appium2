af1f3d52a17175405a849d0d668a2f89c10ca521
ilachowska <iwona.lachowska@rankomat.pl>
2022-04-26 07:54:26 +0200

    Sendkeys methods

--- src/test/BasicTest.java
+++ src/test/BasicTest.java
@@ -1,21 +1,26 @@
 package test;
 
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.pagefactory.ElementLocator;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 import static java.lang.Thread.sleep;
 
 public class BasicTest {
 
-    private AndroidDriver driver;
+    private AppiumDriver driver;
 
     @Before
     public void openApp() throws MalformedURLException {
@@ -30,17 +35,26 @@ public class BasicTest {
         cap.setCapability("ignoreHiddenApiPolicyError", "true");
 
         URL url = new URL("http://localhost:4723/wd/hub");
-        driver = new AndroidDriver(url, cap);
+        driver = new AppiumDriver(url, cap);
         System.out.println("Application started");
     }
 
     @Test
-    public void sampleTest() throws InterruptedException {
+    public void sampleTest() {
+
+        // works with AndroidDriver, AppiumDriver
         WebElement e = driver.findElement(By.xpath("//android.widget.EditText[1]"));
         e.click();
-        sleep(2);
-        driver.executeScript("flutter:enterText", "wwwwww");
-        //e.sendKeys("wwww");
-        sleep(2);
+
+        //method 1
+        //driver.getKeyboard().pressKey("dddd");
+
+        //method 2
+        //Actions action = new Actions(driver);
+        //action.sendKeys(driver.findElement(By.xpath("//android.widget.EditText[1]")), "lll").perform();
+
+        //method 3
+        //driver.getKeyboard().sendKeys("gggggggkjndvck");
+
     }
 }
